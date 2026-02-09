cc252ac5ed857ec2f821967fabeeb9a183c8bd28
Leon <leonwyrobek05@gmail.com>
2026-02-09 21:29:01 +0100

    Implementation of add() and list(). Managing files to not loose tasks

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 #define MAX_STRING_LENGTH 256
 typedef struct Task {
-    char description[MAX_STRING_LENGTH];
     int completed;
+    char description[MAX_STRING_LENGTH];
     struct Task* next;
 } Task;
 
@@ -35,7 +35,18 @@ void add(char *description) {
 }
 
 void list() {
-
+    Task* current = task_list;
+    int i = 0;
+    while (current != NULL) {
+        if (current->completed == 0) {
+            printf("❌");
+        } else {
+            printf("✅");
+        }
+        printf("%s\n", current -> description);
+        current = current -> next;
+        i++;
+    }
 }
 
 void complete() {
@@ -46,9 +57,47 @@ void delete() {
 
 }
 
+void load_file() {
+    FILE* file = fopen("tasks.txt", "r");
+    if (file == NULL) return;
+
+    int status;
+    char desc[256];
+
+    while (fscanf(file, "%d;%[^\n]", &status, desc) == 2) {
+        add(desc);
+
+        Task* current = task_list;
+        while (current -> next != NULL) current = current -> next;
+        current -> completed = status;
+    }
+}
+
+void save_file() {
+    FILE* file = fopen("tasks.txt", "w");
+    if (file == NULL) return;
+
+    Task* current_task = task_list;
+
+    while (current_task != NULL) {
+        fprintf(file, "%d;%s\n", current_task -> completed, current_task -> description);
+        current_task = current_task -> next;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         printf("Invalid command. Use todo -h for help.\n");
     }
+    
+    load_file();
+
+    if (strcmp(argv[1], "add") == 0) {
+        add(argv[2]);
+        save_file();
+    } else if (strcmp(argv[1], "list") == 0) {
+        list();
+    }
+
     return 0;
 }
